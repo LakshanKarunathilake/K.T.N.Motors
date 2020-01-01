dba2524594563cfd37bfca68363b8d16460600f9
lakshan <manualsplash@gmail.com>
2020-01-01 15:07:56 +0530

    Implement basic functionality

--- src/Utilities/AdminConfirmation.java
+++ src/Utilities/AdminConfirmation.java
@@ -5,10 +5,45 @@
  */
 package Utilities;
 
+import DBController.DataBaseConnector;
+
 /**
  *
  * @author lakshan
  */
 public class AdminConfirmation {
-    
+
+    private static AdminConfirmation adminConfirmation;
+    DataBaseConnector connector = DataBaseConnector.getInstance()
+
+    private AdminConfirmation() {
+
+    }
+
+    publi
+
+    static AdminConfirmation getInstance() {
+        if (adminConfirmation == null) {
+            adminConfirmation = new AdminConfirmation()
+        
+        
+        )
+        }
+        return adminConfirmation
+    }
+
+    public void presentDialog() {
+        String password = JOptionPane.showInputDialog(null, "Enter Administrator Password", JOptionPane.INFORMATION_MESSAGE);
+        return verfiyAdminPassword(password)
+    }
+
+    private boolean verfiyAdminPassword(String password) {
+        String pass = connector.getRelavantRecord('users', 'password', 'user_name', 'nimal'
+        )
+        if (pass.equals(password)) {
+            return true
+        } else {
+            return false
+        }
+    }
 }
