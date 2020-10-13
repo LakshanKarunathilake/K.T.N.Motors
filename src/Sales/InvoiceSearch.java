6ac548a4d1f0cc6d41ffba76b9b86528f151d030
lakshan <manualsplash@gmail.com>
2020-10-13 23:03:06 +0530

    replace invoice search values

--- src/Sales/InvoiceSearch.java
+++ src/Sales/InvoiceSearch.java
@@ -21,48 +21,52 @@ import javax.swing.table.DefaultTableModel;
  * @author lakshan
  */
 public class InvoiceSearch {
-    
+
     JTextField invoice_txt;
     JComboBox customerID_combo;
     JComboBox customerName_combo;
+    JComboBox salesRepNameCombo;
     JTable table;
     JTextField totalTxt;
     JTextField discountTxt;
     JTextField grandTxt;
     JTextField additionalNoteTxt;
-    
+
     DataBaseConnector connector;
-    
+
     JCheckBox check;
     JTextField cash_txt;
     JTextField credit_txt;
     JPanel halfpay;
-    
-    public InvoiceSearch(JTextField invoice_txt,JComboBox customerID_combo,JComboBox customerName_combo,JTable table,JTextField totalTxt,JTextField discountTxt,JTextField grandTxt,JTextField additionalNoteTxt,DataBaseConnector connector){
+
+    public InvoiceSearch(JTextField invoice_txt, JComboBox salesRepNameCombo, JComboBox customerID_combo, JComboBox customerName_combo, JTable table, JTextField totalTxt, JTextField discountTxt, JTextField grandTxt, JTextField additionalNoteTxt, DataBaseConnector connector) {
         this.invoice_txt = invoice_txt;
         this.customerID_combo = customerID_combo;
         this.customerName_combo = customerName_combo;
+        this.salesRepNameCombo = salesRepNameCombo;
         this.table = table;
         this.totalTxt = totalTxt;
         this.discountTxt = discountTxt;
         this.grandTxt = grandTxt;
         this.connector = connector;
         this.additionalNoteTxt = additionalNoteTxt;
+        
     }
-    
-    public void setHalfPayComponents(JCheckBox check,JTextField cash_txt,JTextField credit_txt,JPanel halfpay){
+
+    public void setHalfPayComponents(JCheckBox check, JTextField cash_txt, JTextField credit_txt, JPanel halfpay) {
         this.check = check;
         this.credit_txt = credit_txt;
         this.cash_txt = cash_txt;
         this.halfpay = halfpay;
     }
-    
+
     public void fillInvoice(String invoiceID) {
         invoice_txt.setText(invoiceID);
         String userID = connector.getRelavantRecord("invoices", "customer_code", "invoice_id", invoiceID);
         String additionalNote = connector.getRelavantRecord("invoices", "additional_note", "invoice_id", invoiceID);
         customerID_combo.setSelectedItem(userID);
         customerName_combo.setSelectedItem(connector.getRelavantRecord("customers", "name", "customer_code", userID));
+        salesRepNameCombo.setSelectedItem(connector.getRelavantRecord("invoices", "repName", "invoice_id", invoiceID));
 
         ArrayList itemNoList;
         ArrayList itemQtyList;
@@ -72,9 +76,9 @@ public class InvoiceSearch {
         itemTotalList = connector.retreveDataColoumnWithCondition("invoiceitems", "total", "invoice_id", invoiceID);
 
         ViewManipulation.emptyTable(table);
-        
+
         DefaultTableModel model = (DefaultTableModel) table.getModel();
-        
+
         Object rowData[] = new Object[6];
 
         for (int i = 0; i < itemNoList.size(); i++) {
@@ -91,12 +95,12 @@ public class InvoiceSearch {
             model.addRow(rowData);
 
         }
-        
+
         String cash = connector.getRelavantRecord("invoices", "cash_paid", "invoice_id", invoiceID);
         String grandTotal = connector.getRelavantRecord("invoices", "grandTotal", "invoice_id", invoiceID);
         halfpay.setVisible(false);
-        if(!cash.equals(grandTotal)){
-            double credit = Double.parseDouble(grandTotal)- Double.parseDouble(cash);
+        if (!cash.equals(grandTotal)) {
+            double credit = Double.parseDouble(grandTotal) - Double.parseDouble(cash);
             halfpay.setVisible(true);
             check.setSelected(true);
             credit_txt.setText(Rounding.RoundTo5(credit, true));
